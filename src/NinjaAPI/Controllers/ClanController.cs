ece2015c742f687e47ce06d7a7a69ddc1e0f126b
thedung0908 <thedung0908@gmail.com>
2019-04-19 16:23:23 +0700

    add clan controller and model

---
+++ src/NinjaAPI/Controllers/ClanController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NinjaAPI.Models;
+
+namespace NinjaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClanController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Clan>), 200)]
+        public Task<IActionResult> ReadAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/NinjaAPI.Tests/Controllers/ClanControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NinjaAPI.Controllers;
+using NinjaAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NinjaAPI.Tests.Controllers
+{
+    class ClanControllerTest
+    {
+        protected ClanController ControllerUnderTest { get; }
+        public ClanControllerTest ()
+        {
+            ControllerUnderTest = new ClanController();
+        }
+        
+        public class ReadAllAsync : ClanControllerTest
+        {
+            [Fact]
+            public async void ShouldReturnOKObjectResultWithClan()
+            {
+                //Arrange
+                var expectedClans = new Clan[]
+                {
+                    new Clan{ Name = "Test Clan 1"},
+                    new Clan{ Name = "Test Clan 2"},
+                    new Clan{ Name = "Test Clan 3"}
+                };
+
+                //Act
+                var result = await ControllerUnderTest.ReadAllAsync();
+
+                //Assert
+                var okResult = Assert.IsType<OkObjectResult>(result);
+                Assert.Same(expectedClans, okResult.Value);
+            }
+        }
+    }
+}
